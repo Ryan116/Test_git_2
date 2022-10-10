3a57670b02ff662ec288cb9a005628b81fed2d0c
Ruslan Amirov <gumagereiro@rambler.ru>
2022-10-10 15:34:51 +0300

    val_c_created

--- app/src/main/java/com/example/test_git_2/MainActivity.kt
+++ app/src/main/java/com/example/test_git_2/MainActivity.kt
@@ -7,6 +7,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val b = 111
+        val c = 111
     }
 }
